9fee73740cd8aefefdc4f33ea6683b3f98eab3df
staticBunny <fatehbir2000@gmail.com>
2022-10-07 23:30:26 +0530

    Rewrite functions

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-function add(a, b){
-    return a + b;
-}
+'use strict';
 
-function subtract(a, b) {
-    return a - b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, a, b) {
-    return operator(a, b);
-}
+const operate = (operator, a, b) => operator(a, b);
